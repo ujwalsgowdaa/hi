7090e1fdf66d5b8aab31d1a80f9785a2b03c8814
Lenovo@UJWAL <ujwals1818@gmail.com.com>
2024-04-08 18:10:37 +0530

    hello hi

---
+++ src/main/java/com/myblog22/demoblog22/Demoblog22Application.java
@@ -0,0 +1,13 @@
+package com.myblog22.demoblog22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoblog22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoblog22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog22/demoblog22/Demoblog22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog22.demoblog22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoblog22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
